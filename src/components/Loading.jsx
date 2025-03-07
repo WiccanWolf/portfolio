5a929a996b34b35402a2628bbe1cf07d2bd32348
Toni <dayhlprod@gmail.com>
2025-03-07 09:34:51 +0000

    Fixed broken dependencies

--- src/components/Loading.jsx
+++ src/components/Loading.jsx
@@ -1,19 +1,9 @@
 import React from 'react';
-import { ThreeDots } from 'react-loader-spinner';
 
 const Loading = () => {
   return (
     <div className='loading-box'>
       <p>Page is loading, please wait.</p>
-      <ThreeDots
-        className='loading'
-        visible={true}
-        height='80'
-        width='80'
-        color='black'
-        radius='9'
-        ariaLabel='three-dots-loading'
-      />
     </div>
   );
 };
